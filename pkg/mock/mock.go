37264ccacd6e5f42f0377fb174ad28be747a5339
Russell Rollins <russell1124@gmail.com>
2020-02-05 12:05:39 -0500

    Add tests of mock server.

--- pkg/mock/mock.go
+++ pkg/mock/mock.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 
 	"github.com/go-icap/icap"
@@ -16,22 +17,58 @@ type Transformer interface {
 	Transform(r io.Reader) (t io.Reader, err error)
 }
 
+// Option is a configuration option for passing to the Artifactory constructor.
+// This is used to implement the "Functional Options" pattern:
+//    https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
+type Option func(*MockServer) error
+
 type MockServer struct {
+	mockFilesRoot string
+
 	icapPort int
 	apiPort  int
 
 	transformers []Transformer
 }
 
-func NewMockServer() (*MockServer, error) {
-	return &MockServer{
+func NewMockServer(options ...Option) (*MockServer, error) {
+	ms := &MockServer{
+		mockFilesRoot: "/mocks",
+
 		icapPort: 11344,
 		apiPort:  80,
+	}
 
-		transformers: []Transformer{
-			&VariableSubstitution{key: "Name", value: "Russell"},
-		},
-	}, nil
+	for _, o := range options {
+		if err := o(ms); err != nil {
+			return nil, err
+		}
+	}
+
+	_, err := os.Open(ms.mockFilesRoot)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"invalid mock file directory %v: %w", ms.mockFilesRoot, err,
+		)
+	}
+
+	return ms, nil
+}
+
+func WithMockRoot(root string) Option {
+	return func(m *MockServer) error {
+		m.mockFilesRoot = root
+		return nil
+	}
+}
+
+func WithDefaultVariables(vars ...*VariableSubstitution) Option {
+	return func(m *MockServer) error {
+		for _, newVar := range vars {
+			m.addVariableSubstitution(newVar)
+		}
+		return nil
+	}
 }
 
 func (ms *MockServer) Serve() error {
@@ -48,7 +85,7 @@ func (ms *MockServer) Serve() error {
 
 	// We also want to gracefully stop when the OS asks us to
 	killSignal := make(chan os.Signal, 1)
-	signal.Notify(killSignal, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(killSignal, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR2)
 
 	go func() {
 		icapErrC <- icap.ListenAndServe(fmt.Sprintf(":%d", ms.icapPort), nil)
@@ -96,9 +133,17 @@ func (ms *MockServer) interception(w icap.ResponseWriter, req *icap.Request) {
 }
 
 func (ms *MockServer) mockHandler(w http.ResponseWriter, r *http.Request) {
-	mock, err := os.Open("mocks/example.mock")
+	path := r.URL.Path
+	if r.URL.Path == "/" {
+		path = "index"
+	}
+
+	mockPath := filepath.Join(ms.mockFilesRoot, r.URL.Host, path)
+	fileName := fmt.Sprintf("%s.mock", mockPath)
+
+	mock, err := os.Open(fileName)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("failed opening mock file: %s", err.Error()), 500)
+		http.Error(w, fmt.Sprintf("failed opening mock file: %s", err.Error()), http.StatusNotFound)
 		return
 	}
 
@@ -163,9 +208,15 @@ func (ms *MockServer) substitutionVariableHandler(
 	case http.MethodPost:
 		err := r.ParseMultipartForm(4096)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(
+				w,
+				fmt.Sprintf("error parsing input form: %s", err.Error()),
+				http.StatusInternalServerError,
+			)
 			return
 		}
+
+		fmt.Printf("%+v\n", r.PostForm)
 
 		key := r.PostForm.Get("key")
 		value := r.PostForm.Get("value")
---
+++ pkg/mock/mock_test.go
@@ -0,0 +1,307 @@
+package mock
+
+import (
+	"bytes"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewMockServer(t *testing.T) {
+	tcs := []struct {
+		name    string
+		options []Option
+		want    *MockServer
+	}{
+		{
+			name: "simple",
+			options: []Option{
+				WithMockRoot("testdata/"),
+			},
+			want: &MockServer{
+				apiPort:  80,
+				icapPort: 11344,
+
+				mockFilesRoot: "testdata/",
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		tc := tc // capture range variable
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := NewMockServer(tc.options...)
+			require.Nil(t, err)
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestMockServerMockHandler(t *testing.T) {
+	tcs := []struct {
+		name    string
+		options []Option
+		url     string
+		want    string
+	}{
+		{
+			name: "simple",
+			url:  "example.com/simple",
+			options: []Option{
+				WithMockRoot("testdata/"),
+			},
+			want: "Hello, World!\n",
+		},
+		{
+			name: "substitutions",
+			url:  "example.com/substitutions",
+			options: []Option{
+				WithMockRoot("testdata/"),
+				WithDefaultVariables(
+					&VariableSubstitution{key: "name", value: "Davenport"},
+				),
+			},
+			want: "Hello, Davenport!\n",
+		},
+	}
+
+	for _, tc := range tcs {
+		tc := tc // capture range variable
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ms, err := NewMockServer(tc.options...)
+			require.Nil(t, err)
+
+			req, err := http.NewRequest(http.MethodGet, tc.url, nil)
+			require.Nil(t, err)
+
+			recorder := httptest.NewRecorder()
+
+			ms.mockHandler(recorder, req)
+
+			assert.Equal(t, http.StatusOK, recorder.Result().StatusCode)
+
+			gotBytes, err := ioutil.ReadAll(recorder.Result().Body)
+			require.Nil(t, err)
+			got := string(gotBytes)
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestMockServerSubstitutionVariableHandler_GET(t *testing.T) {
+	tcs := []struct {
+		name    string
+		options []Option
+		want    string
+	}{
+		{
+			name: "simple",
+			options: []Option{
+				WithMockRoot("testdata/"),
+				WithDefaultVariables(
+					&VariableSubstitution{key: "name", value: "Davenport"},
+				),
+			},
+			want: `[{"key":"name","value":"Davenport"}]`,
+		},
+		{
+			name: "multi",
+			options: []Option{
+				WithMockRoot("testdata/"),
+				WithDefaultVariables(
+					&VariableSubstitution{key: "name", value: "Davenport"},
+					&VariableSubstitution{key: "name", value: "Barry"},
+					&VariableSubstitution{key: "foo", value: "bar"},
+				),
+			},
+			want: `[{"key":"name","value":"Barry"},{"key":"foo","value":"bar"}]`,
+		},
+	}
+
+	for _, tc := range tcs {
+		tc := tc // capture range variable
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ms, err := NewMockServer(tc.options...)
+			require.Nil(t, err)
+
+			req, err := http.NewRequest(http.MethodGet, "", nil)
+			require.Nil(t, err)
+
+			recorder := httptest.NewRecorder()
+
+			ms.substitutionVariableHandler(recorder, req)
+
+			assert.Equal(t, http.StatusOK, recorder.Result().StatusCode)
+
+			gotBytes, err := ioutil.ReadAll(recorder.Result().Body)
+			require.Nil(t, err)
+			got := string(gotBytes)
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestMockServerSubstitutionVariableHandler_POST(t *testing.T) {
+	tcs := []struct {
+		name    string
+		options []Option
+		key     string
+		value   string
+		want    []Transformer
+	}{
+		{
+			name:  "simple",
+			key:   "name",
+			value: "Davenport",
+			options: []Option{
+				WithMockRoot("testdata/"),
+			},
+			want: []Transformer{
+				&VariableSubstitution{key: "name", value: "Davenport"},
+			},
+		},
+		{
+			name:  "replace",
+			key:   "name",
+			value: "Barry",
+			options: []Option{
+				WithMockRoot("testdata/"),
+				WithDefaultVariables(
+					&VariableSubstitution{key: "name", value: "Davenport"},
+				),
+			},
+			want: []Transformer{
+				&VariableSubstitution{key: "name", value: "Barry"},
+			},
+		},
+		{
+			name:  "add",
+			key:   "foo",
+			value: "bar",
+			options: []Option{
+				WithMockRoot("testdata/"),
+				WithDefaultVariables(
+					&VariableSubstitution{key: "name", value: "Davenport"},
+				),
+			},
+			want: []Transformer{
+				&VariableSubstitution{key: "name", value: "Davenport"},
+				&VariableSubstitution{key: "foo", value: "bar"},
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		tc := tc // capture range variable
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ms, err := NewMockServer(tc.options...)
+			require.Nil(t, err)
+
+			var formBody bytes.Buffer
+			formWriter := multipart.NewWriter(&formBody)
+			_ = formWriter.WriteField("key", tc.key)
+			_ = formWriter.WriteField("value", tc.value)
+			formWriter.Close()
+
+			req, err := http.NewRequest(http.MethodPost, "", &formBody)
+			require.Nil(t, err)
+			req.Header.Set("Content-Type", formWriter.FormDataContentType())
+
+			recorder := httptest.NewRecorder()
+
+			ms.substitutionVariableHandler(recorder, req)
+
+			if !assert.Equal(t, http.StatusOK, recorder.Result().StatusCode) {
+				resBytes, err := ioutil.ReadAll(recorder.Result().Body)
+				require.Nil(t, err)
+				res := string(resBytes)
+				require.Fail(t, res)
+			}
+
+			got := ms.transformers
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestMockServerAddVariableSubstitution(t *testing.T) {
+	tcs := []struct {
+		name          string
+		options       []Option
+		substitutions []*VariableSubstitution
+		want          []Transformer
+	}{
+		{
+			name: "simple",
+			options: []Option{
+				WithMockRoot("testdata/"),
+			},
+			substitutions: []*VariableSubstitution{
+				{key: "foo", value: "bar"},
+			},
+			want: []Transformer{
+				&VariableSubstitution{key: "foo", value: "bar"},
+			},
+		},
+		{
+			name: "adding with different key adds",
+			options: []Option{
+				WithMockRoot("testdata/"),
+			},
+			substitutions: []*VariableSubstitution{
+				{key: "foo", value: "bar"},
+				{key: "bing", value: "baz"},
+			},
+			want: []Transformer{
+				&VariableSubstitution{key: "foo", value: "bar"},
+				&VariableSubstitution{key: "bing", value: "baz"},
+			},
+		},
+		{
+			name: "adding with same key overrides",
+			options: []Option{
+				WithMockRoot("testdata/"),
+			},
+			substitutions: []*VariableSubstitution{
+				{key: "foo", value: "bar"},
+				{key: "foo", value: "baz"},
+			},
+			want: []Transformer{
+				&VariableSubstitution{key: "foo", value: "baz"},
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		tc := tc // capture range variable
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ms, err := NewMockServer(tc.options...)
+			require.Nil(t, err)
+
+			for _, s := range tc.substitutions {
+				ms.addVariableSubstitution(s)
+			}
+
+			got := ms.transformers
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
