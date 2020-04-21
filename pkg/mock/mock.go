3735047892c96602dcede23a2cd5c8f2087bd308
Russell Rollins <russell1124@gmail.com>
2020-04-21 11:48:19 -0400

    Make certificates optional, instead of mandatory.

--- pkg/mock/mock.go
+++ pkg/mock/mock.go
@@ -242,14 +242,15 @@ func (ms *MockServer) mockHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ms.logger.Info("parsing URL", "route", route, "url", r.URL)
+	ms.logger.Info("parsing URL", "route", fmt.Sprintf("%+v", route), "url", r.URL)
 	path, localTransformers, err := route.ParseURL(r.URL)
 	if err != nil {
 		ms.logger.Error("failed to parse mock URL for route", "error", err.Error())
 		http.Error(w, fmt.Sprintf("failed to parse mock URL for route: %s",
 			err.Error()), http.StatusInternalServerError)
 	}
-	ms.logger.Info("parsed URL", "path", path)
+	ms.logger.Info("parsed URL produced path", "path", path)
+
 	switch route.Type {
 	case "http":
 		ms.logger.Info("detected an http mock attempt")
